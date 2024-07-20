2853ebc0972145913dff35a19e872ec9ca9b3961
limlx233 <2958479737@qq.com>
2024-07-20 13:45:20 +0800

    "dashboardTest"

--- DataVisualizationProj/main.py
+++ DataVisualizationProj/main.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 
+st.set_page_config(layout='wide',initial_sidebar_state='expanded')
 
 homepage = st.Page("streamlit/HomePage.py",title="Home",icon=":material/home:")
 dashboard = st.Page("streamlit/Dashboard.py",title="Dashboard",icon=":material/dashboard:")
--- DataVisualizationProj/streamlit/Dashboard.py
+++ DataVisualizationProj/streamlit/Dashboard.py
@@ -1,15 +1,24 @@
-import streamlit as st 
-from chartsFile.charts import *
-# st.write('dashboard todo')
+import streamlit as st
+import os
+import base64
+import streamlit.components.v1 as components
 
+@st.cache_data
+def load_html():
+    # 获取 file1 的路径
+    file1_path = os.path.dirname(__file__)
+    # 生成 file2 中 HTML 文件的路径
+    dashboard_html_path = os.path.join(file1_path, '../frontendFile/dashboard.html')
+    # 打开并读取 HTML 文件
+    with open(dashboard_html_path, 'r', encoding='utf-8') as HtmlFile:
+        raw_html = HtmlFile.read().encode("utf-8")
+        raw_html = base64.b64encode(raw_html).decode()
+    return raw_html
+
+st.title('Dashboard')
+st.header('Just a test!', divider="rainbow")
+
+raw_html = load_html()
 with st.container(border=True):
-    st.header('Dashboard',divider='rainbow')
-    col1, col2, col3 = st.columns(3,gap="medium")
-    with col1:
-        st.components.v1.html(liquid_chart1(), height=200)
-    with col2:
-        st.components.v1.html(liquid_chart2(), height=200)
-    with col3:
-        st.components.v1.html(liquid_chart3(), height=200)
-    
-    
+    with st.spinner('加载中...'):
+        components.iframe(f"data:text/html;base64,{raw_html}", height=1400, width=900)
