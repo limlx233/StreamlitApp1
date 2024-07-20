7a4359e9da5e724411a3529fc5851d71b9ab2d18
limlx233 <2958479737@qq.com>
2024-07-20 22:37:52 +0800

    "增加requirements依赖"

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import pyecharts as pc
-import streamlit as st  
+import streamlit as st 
+import matplotlib as mt 
 
 print(f"Python 版本：3.9.16")
 print(f"pandas 版本：{pd.__version__}")
 print(f"numpy 版本：{np.__version__}")
 print(f"pyecharts 版本：{pc.__version__}")
 print(f"streamlit 版本：{st.__version__}")
+print(f"matplotlib 版本： {mt.__version__}")
