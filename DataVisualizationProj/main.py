ad7b43e9c1d946c15c8a45df55a597870d4e5dcb
limlx233 <2958479737@qq.com>
2024-07-20 22:52:31 +0800

    "增加主页功能说明"

--- DataVisualizationProj/main.py
+++ DataVisualizationProj/main.py
@@ -2,17 +2,17 @@ import streamlit as st
 
 st.set_page_config(layout='wide',initial_sidebar_state='expanded')
 
-homepage = st.Page("streamlit/HomePage.py",title="Home",icon=":material/home:")
-dashboard = st.Page("streamlit/Dashboard.py",title="Dashboard",icon=":material/dashboard:")
-dataexplore = st.Page("streamlit/DataExplore.py",title="Explore",icon=":material/explore:")
-login= st.Page("streamlit/login.py",title="Login",icon=":material/login:")
-logout= st.Page("streamlit/logout.py",title="Logout",icon=":material/logout:")
+homepage = st.Page("streamlit/HomePage.py",title="主页",icon=":material/home:")
+dashboard = st.Page("streamlit/Dashboard.py",title="数据看板",icon=":material/dashboard:")
+dataexplore = st.Page("streamlit/DataExplore.py",title="数据处理",icon=":material/explore:")
+login= st.Page("streamlit/login.py",title="登录",icon=":material/login:")
+logout= st.Page("streamlit/logout.py",title="登出",icon=":material/logout:")
 
 
 pg = st.navigation(
     {
-        "HomePage":[homepage,dashboard,dataexplore],
-        "UserInfo":[login,logout],
+        "主页":[homepage,dashboard,dataexplore],
+        "用户信息":[login,logout],
     }
 )
 
--- DataVisualizationProj/streamlit/HomePage.py
+++ DataVisualizationProj/streamlit/HomePage.py
@@ -1,4 +1,7 @@
 import streamlit as st 
 
 
-st.title('HomePage')
+st.title('主页')
+st.header('数据应用说明', divider="rainbow")
+st.subheader('数据可视化')
+st.subheader('数据处理')
